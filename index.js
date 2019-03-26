33e64b5f646615388caa5632257209c8a9c9e667
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-26 12:18:19 -0700

    Scaffold API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+
+const zoosController = require("./controllers/zoos");
 
 const server = express();
 
@@ -7,8 +9,9 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.use("/api/zoos", zoosController);
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+	console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
