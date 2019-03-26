f33915f158af4afcf9074b65e4e4f0760c45a881
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-26 12:31:12 -0700

    Handle get zoos endpoint

--- controllers/zoos.js
+++ controllers/zoos.js
@@ -4,11 +4,22 @@ const db = require("knex")({
 	client: "sqlite3",
 	useNullAsDefault: true,
 	connection: {
-		filename: "data/roles.db"
+		filename: "data/lambda.sqlite3"
 	}
 });
 
 // Get zoo
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+	try {
+		const zoos = await db("zoos");
+
+		res.status(200).json(zoos);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Sorry, something went wrong trying to get all zoos"
+		});
+	}
+});
 
 module.exports = router;
