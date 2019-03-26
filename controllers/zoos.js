40db86a3de769a6e51177f1f2a05d4685bfa8bf8
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-26 12:58:57 -0700

    Handle delete zoo endpoint

--- controllers/zoos.js
+++ controllers/zoos.js
@@ -80,6 +80,24 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete zoo
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const zoo = await db("zoos")
+			.where({ id })
+			.del();
+
+		if (zoo > 0) return res.status(200).json(zoo);
+
+		return res
+			.status(404)
+			.json({ message: "Sorry, that zoo could not be found" });
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, there was a problem deleting that zoo "
+		});
+	}
+});
 
 module.exports = router;
