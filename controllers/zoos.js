7e117d70d9b0d6e6c963a8e1da8324949f150094
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-26 12:35:06 -0700

    Handle get zoo by id endpoint

--- controllers/zoos.js
+++ controllers/zoos.js
@@ -23,6 +23,20 @@ router.get("/", async (req, res) => {
 });
 
 // Get zoo by id
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const zoo = await db("zoos")
+			.first()
+			.where({ id });
+
+		res.status(200).json(zoo);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, there was a problem while retrieving that zoo"
+		});
+	}
+});
 
 module.exports = router;
